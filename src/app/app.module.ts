63b4e9477e381c2a5fccbf5c713715d38c488f8f
unknown <damjan.trifunovic@gmail.com>
2018-06-12 00:04:44 +0200

    add question info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,34 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import {MatMenuModule, MatButtonModule, MatFormFieldModule, MatIconModule, MatToolbarModule } from '@angular/material'
+import {
+  MatMenuModule, 
+  MatButtonModule, 
+  MatFormFieldModule,
+  MatIconModule, 
+  MatToolbarModule, 
+  MatDividerModule,
+  MatChipsModule
+} from '@angular/material';
+import { QuestionInfoComponent } from './components/question-info/question-info.component';
+import { QuestionListComponent } from './components/question-list/question-list.component'
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    QuestionInfoComponent,
+    QuestionListComponent
   ],
   imports: [
     BrowserModule, 
     BrowserAnimationsModule,
-    MatMenuModule, MatButtonModule, MatFormFieldModule, MatIconModule, MatToolbarModule
+    MatMenuModule, 
+    MatButtonModule, 
+    MatFormFieldModule, 
+    MatIconModule, 
+    MatToolbarModule,
+    MatDividerModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
